9394b48a1bee6333e187460045d8996c8974706f
kurt196794 <kenta.kume1988@gmail.com>
2021-05-23 20:02:32 +0900

    商品一覧表示機能訂正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,]
 
   def index
-    @items = Item.order("created_at DESC")
-    @items = Item.all
+    @items = Item.all.order("created_at DESC")
   end
 
 # def move_to_index
