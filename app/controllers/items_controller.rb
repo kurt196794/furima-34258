2514d310de9bfbe769361fa4c596024ac3fa5043
kurt196794 <kenta.kume1988@gmail.com>
2021-05-25 23:00:35 +0900

    商品情報編集機能１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,21 +1,28 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
-
+ # before_action :contributor_confirmation, only: [:edit, :update]
+  
   def index
     @items = Item.all.order("created_at DESC")
   end
 
-# def move_to_index
-#   redirect_to action: :index unless user_signed_in?
-# end
+  def update
+        @item = Item.find(params[:id])
+    if  @item.update(item_params)
+        redirect_to  item_path(@item)
+   else
+      render :edit
+   end
+ end
 
   def  new
     @item = Item.new
    end
   
-   #def edit
-    # @item = Item.find(params[:id])
-   #end
+   def edit
+     @item = Item.find(params[:id])
+     redirect_to root_path unless current_user.id == @item.user_id
+   end
 
   def create
     @item = Item.new(item_params)
